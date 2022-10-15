8dbc7a23aadc6dbecd0cc648ea969ae43f62deed
Nikhil Badyal <nikhill773384@gmail.com>
2022-10-15 16:45:13 +0530

    ✨ Rm Workflows

---
+++ rm-workflows.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# shellcheck disable=SC2207
+workflow_ids=($(gh api repos/"$GITHUB_REPOSITORY"/actions/workflows | jq '.workflows[] | .id'))
+for workflow_id in "${workflow_ids[@]}"
+do
+  echo "Listing runs for the workflow ID $workflow_id"
+  run_ids=($(gh api repos/"$GITHUB_REPOSITORY"/actions/workflows/"$workflow_id"/runs --paginate | jq '.workflow_runs[].id'))
+  for run_id in "${run_ids[@]}"
+  do
+    echo "Deleting Run ID $run_id"
+    gh api repos/"$GITHUB_REPOSITORY"/actions/runs/"$run_id" -X DELETE >/dev/null
+  done
+done
