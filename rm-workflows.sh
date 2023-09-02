b9e504fb70a201fa67b508d47e558e56f7de419f
Nikhil Badyal <nikhill773384@gmail.com>
2023-09-02 16:16:18 +0530

    🎨 Added GitHub config

--- rm-workflows.sh
+++ rm-workflows.sh
@@ -4,10 +4,17 @@ workflow_ids=($(gh api repos/"$GITHUB_REPOSITORY"/actions/workflows | jq '.workf
 for workflow_id in "${workflow_ids[@]}"
 do
   echo "Listing runs for the workflow ID $workflow_id"
-  run_ids=($(gh api repos/"$GITHUB_REPOSITORY"/actions/workflows/"$workflow_id"/runs --paginate | jq '.workflow_runs[].id'))
+  run_ids=($(gh api repos/"$GITHUB_REPOSITORY"/actions/workflows/"$workflow_id"/runs?status=completed --paginate | jq '.workflow_runs[].id'))
   for run_id in "${run_ids[@]}"
   do
     echo "Deleting Run ID $run_id"
     gh api repos/"$GITHUB_REPOSITORY"/actions/runs/"$run_id" -X DELETE >/dev/null
   done
+done
+echo "Deleting orphan workflows"
+run_ids=($(gh api repos/"$GITHUB_REPOSITORY"/actions/runs?status=completed --paginate | jq '.workflow_runs[].id'))
+for run_id in "${run_ids[@]}"
+do
+  echo "Deleting Run ID $run_id"
+  gh api repos/"$GITHUB_REPOSITORY"/actions/runs/"$run_id" -X DELETE >/dev/null
 done
